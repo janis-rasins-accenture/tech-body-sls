0170fb5c3155c018058b082a4b69bff867c27b1e
Lejiete <linda.lejiete@accenture.com>
2023-12-06 11:16:28 +0200

    port from 8011 changed back to 8010

--- src/aws/dynamodb/lib/ddbClient.ts
+++ src/aws/dynamodb/lib/ddbClient.ts
@@ -1,7 +1,7 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 
 const REGION = process.env.REGION ?? 'eu-west-1';
-const ENDPOINT = process.env.DYNAMO_ENDPOINT ?? 'http://localhost:8011';
+const ENDPOINT = process.env.DYNAMO_ENDPOINT ?? 'http://localhost:8010';
 
 export const ddbClient = new DynamoDBClient({
   region: REGION,
