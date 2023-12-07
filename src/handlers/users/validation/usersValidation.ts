de9d801957b8eeadf4011a66915707330c135f96
Janis Rasins <extern.janis.rasins@vwfs.io>
2023-12-07 14:39:05 +0200

    Add Create user

---
+++ src/handlers/users/validation/usersValidation.ts
@@ -0,0 +1,44 @@
+import * as Yup from 'yup';
+import { InputUserCreateIF } from '../../../types/users';
+
+const MIN_NAME_LETTERS = 2;
+const MAX_NAME_LETTERS = 20;
+
+export const userCreateSchema: Yup.ObjectSchema<InputUserCreateIF> = Yup.object(
+  {
+    firstName: Yup.string()
+      .required('First name is required!')
+      .min(
+        MIN_NAME_LETTERS,
+        `Name must be at least ${MIN_NAME_LETTERS} letters`
+      )
+      .max(
+        MAX_NAME_LETTERS,
+        `Name must not exceed ${MAX_NAME_LETTERS} letters`
+      ),
+    lastName: Yup.string()
+      .required('Surname is required!')
+      .min(
+        MIN_NAME_LETTERS,
+        `Surname must be at least ${MIN_NAME_LETTERS} letters`
+      )
+      .max(
+        MAX_NAME_LETTERS,
+        `Surname must not exceed ${MAX_NAME_LETTERS} letters`
+      ),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Please input valid email'),
+    avatarUrl: Yup.string(),
+    userName: Yup.string()
+      .required('Username is required!')
+      .min(
+        MIN_NAME_LETTERS,
+        `Username must be at least ${MIN_NAME_LETTERS} letters`
+      )
+      .max(
+        MAX_NAME_LETTERS,
+        `Username must not exceed ${MAX_NAME_LETTERS} letters`
+      ),
+  }
+);
